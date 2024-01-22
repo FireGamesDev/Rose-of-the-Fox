b8cc7c99789ef88c2e5cf2dbc2cdb543f8ee06da
FireGamesDev <86660115+FireGamesDev@users.noreply.github.com>
2024-01-23 00:21:07 +0100

    eyes done

--- Assets/Scripts/Managers/EyeManager.cs
+++ Assets/Scripts/Managers/EyeManager.cs
@@ -12,10 +12,12 @@ namespace Scripts.Managers
         [SerializeField] private Animator _anim;
         [SerializeField] private GameObject eye;
 
+        private SpriteRenderer eyeSprite;
+
         private float minScale = 1f;
-        private float maxScale = 3f;
-        [SerializeField] private float minDistance = 5f;
-        [SerializeField] private float maxDistance = 20f;
+        private float maxScale = 4.2f;
+        private float minDistance = 2f;
+        private float maxDistance = 17.5f;
         private float blinkInterval = 1f;
 
         private float scaleVariety;
@@ -24,7 +26,11 @@ namespace Scripts.Managers
         {
             player = GameManager.instance.Player;
 
+            eyeSprite = eye.GetComponent<SpriteRenderer>();
+
             scaleVariety = Random.Range(1f, 2f);
+
+            transform.localScale = Vector3.one * scaleVariety;
 
             InvokeRepeating(nameof(Blink), 2f, blinkInterval);
         }
@@ -33,13 +39,13 @@ namespace Scripts.Managers
         {
             float distance = Vector2.Distance(player.transform.position, transform.position);
 
-            float scale = Mathf.Lerp(minScale, maxScale, 1 - Mathf.InverseLerp(minDistance, maxDistance, distance));
+            float scale = Mathf.Lerp(minScale, maxScale, 1 - Mathf.InverseLerp(minDistance, maxDistance, distance)) * scaleVariety;
             transform.DOScale(scale, 0.5f);
 
             float lerpValue = Mathf.InverseLerp(minDistance, maxDistance, distance);
 
-            Color lerpedColor = Color.Lerp(Color.yellow, Color.red, lerpValue);
-            eye.GetComponent<SpriteRenderer>().color = lerpedColor;
+            Color lerpedColor = Color.Lerp(Color.red, Color.yellow, lerpValue);
+            eyeSprite.color = lerpedColor;
 
             if (distance > 5)
             {
